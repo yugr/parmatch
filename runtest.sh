b67e5a364dfd56bea98bdfede38c90ac02b55d02
Yury Gribov <tetra2005@gmail.com>
2016-10-17 06:18:19 +0100

    Assume empty stderr by default.

--- runtest.sh
+++ runtest.sh
@@ -2,22 +2,30 @@
 
 cd `dirname $0`
 
+tmp=$(mktemp)
+
+trap "rm -f $tmp" EXIT
+
 fail=0
 for d in tests/*; do
   if test -d $d; then
     out=$d.out
     err=$d.err
     echo "Testing $d..."
-    tmp=$(mktemp)
     ./parmatch.pl $d >$tmp.out 2>$tmp.err
     if ! diff -q $d/out.txt $tmp.out; then
       echo >&2 "$d: stdout comparison failed:"
       diff -u $d/out.txt $tmp.out
       fail=1
     fi
-    if ! diff -q $d/err.txt $tmp.err; then
+    if test -f $d/err.txt; then
+      stderr=$d/err.txt
+    else
+      stderr=/dev/null
+    fi
+    if ! diff -q $stderr $tmp.err; then
       echo >&2 "$d: stderr comparison failed"
-      diff -u $d/err.txt $tmp.err
+      diff -u $stderr $tmp.err
       fail=1
     fi
   fi
